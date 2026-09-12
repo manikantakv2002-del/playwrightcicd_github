b5759e0f7b08a2df71187f86d163cf46aad2aa4c
Manikanta <manikantakv2002@gmail.com>
2026-09-12 13:08:30 +0530

    Login Test Added

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,7 +2,8 @@ import { test } from "@playwright/test";
 
 test("login page", async ({ page }) => {
     await page.goto("http://orangehrm.qedgetech.com");
-    await page.locator("#txtUsername").fill("Admin");
-    await page.locator("#txtPassword").fill("Qedge123!@#");
-    await page.locator("#btnLogin").click();
+   await page.locator("#txtUsername").fill("Admin");
+   await page.locator("#txtPassword").fill("admin123");
+   await page.locator("#btnLogin").click();
+   await page.waitForTimeout(5000);
 });
